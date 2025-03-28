1896a5942795e0eb945053604b40ec5b4ae31914
Benjamin <Benjaminfwahl@gmail.com>
2025-03-28 10:31:05 -0400

    Created node.java class, started basic framework of both classes.

---
+++ node.java
@@ -0,0 +1,15 @@
+public class node{
+    int x;
+    int y;
+    public node(int x, int y){
+        this.x = x;
+        this.y = y;
+    }
+    public static void main(String[] args) {
+        node node1 = new node(5, 7);
+        System.out.println(node1.get_pos());
+    }
+    public int[] get_pos(){
+        return new int[]{this.x, this.y};
+    }
+}
--- tropical_analysis.java
+++ tropical_analysis.java
@@ -1,10 +1,23 @@
+
+import java.util.*;
+
 public class tropical_analysis {
 
-    public tropical_analysis{
+    public tropical_analysis(){
 
     }
-
-    public static void main(Str[] args){
-        System.out.println("Hello World");
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        System.out.println("How many nodes?");
+        int num_nodes = sc.nextInt();
+        System.out.println("Enter nodes in format [x, y].");
+        
+        for(int i = 0; i < num_nodes; i++){
+            System.out.println();
+            System.out.print("Node: ");
+        }
+    
     }
+
+    
 }
